910fa0c1c111026b6df8dbb1a6295319818aab85
Pratyush <pratyush041002@gmail.com>
2025-03-16 17:43:49 +0530

    Modified Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router";
+import { CircleUserRound, Search, ChevronDown } from 'lucide-react'
+
 function Navbar(){
     return(
         <> 
-            <div id = "navbar" className = "w-screen h-[12vh] flex-row flex justify-around items-center text-center">
+            <div id = "navbar" className = "w-screen h-[12vh] flex-row flex justify-between items-center text-center px-8">
                 <div id = "navbar-items">
-                    <ul className = "list-none flex flex-row gap-4">
+                    <ul className = "list-none flex flex-row gap-6">
                         <li><Link to = "/">Home</Link></li>
                         <li><Link to = "/find">Find Item</Link></li>
                         <li><Link to = "/post">Post Item</Link></li>
@@ -14,15 +16,21 @@ function Navbar(){
                 </div>
 
                 <div className = "">
-                    <h1 className = "text-[24px] font-extrabold text-[#2F27CE]">Campus</h1>
-                    <h2 className = "text-[24px] font-extrabold text-[#2F27CE]">Exchange</h2>
+                    <Link to ="/">
+                        <h1 className = "text-[24px] font-extrabold text-[#2F27CE]">Campus</h1>
+                        <h2 className = "text-[24px] font-extrabold text-[#2F27CE]">Exchange</h2>
+                    </Link>
                 </div>
 
-                <div>
-                    <label htmlFor="">Search</label>
-                    <input type="search" name="" id="" placeholder = "Find item" className = "ml-4 border-2 pl-2 rounded-[4px] mr-8"/>
-                    
-                    <span>Profile</span>
+                <div className = "flex flex-row">
+                    <Search size={18} className = "relative left-10 top-2 z-10" color="#2F27CE"/>
+                    <input type="search" name="" id="" placeholder = "Find item" className = "ml-4 border-2 pl-8 rounded-[4px] mr-4 w-70 outline-none border-[#2F27CE]"/>
+                    <button className = "px-8 py-1 bg-[#2F27CE] text-white rounded-[8px] cursor-pointer hover:bg-[#2F27CE] font-[600]">Search</button>
+                </div>
+
+                <div className = "flex flex-row gap-2 cursor-pointer" id = "profile">
+                    <span><CircleUserRound size={28} color="#2F27CE"/></span>
+                    <span id = "arrow"><ChevronDown size = {24} color="#2F27CE"/></span>
                 </div>
             </div>
         </>
